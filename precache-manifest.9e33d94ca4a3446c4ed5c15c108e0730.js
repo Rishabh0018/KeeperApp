3761bf64f7fba638a193f1951e7ab5aeca04af98
Rishabh Kamthare <rkamthare98@gmail.com>
2023-05-26 18:14:01 +0530

    Updates

---
+++ precache-manifest.9e33d94ca4a3446c4ed5c15c108e0730.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd98434d26539fb711bd75880a08bdb4",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "6034659cae5aa65aba77",
+    "url": "/KeeperApp/static/js/2.0ae6444a.chunk.js"
+  },
+  {
+    "revision": "1e44cd8a0f5e3a321556",
+    "url": "/KeeperApp/static/js/main.fa8f8053.chunk.js"
+  },
+  {
+    "revision": "1c2d34ffb0273d07c705",
+    "url": "/KeeperApp/static/js/runtime-main.41abc274.js"
+  }
+]);
